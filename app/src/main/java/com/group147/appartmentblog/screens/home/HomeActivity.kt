e34cd5bc7d4916524e2347fc78d85df9e8303cb7
Benli <104165100+benliSZ@users.noreply.github.com>
2025-02-26 23:37:27 +0200

    Revert "Feature/ma 23/create navbar"

--- app/src/main/java/com/group147/appartmentblog/screens/home/HomeActivity.kt
+++ app/src/main/java/com/group147/appartmentblog/screens/home/HomeActivity.kt
@@ -2,25 +2,18 @@ package com.group147.appartmentblog.screens.home
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.group147.appartmentblog.R
 import com.group147.appartmentblog.screens.login.LoginActivity
 
 class HomeActivity : AppCompatActivity() {
-    var navController: NavController? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle
+    ?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
@@ -31,19 +24,6 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val navHostFragment: NavHostFragment? = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
-        navController = navHostFragment?.navController
-
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        navController?.let { NavigationUI.setupWithNavController(bottomNavigationView, it) }
-    }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.navbar_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        navController?.let { NavigationUI.onNavDestinationSelected(item, it) }
-        return true
     }
 
     fun onLogoutClicked() {
