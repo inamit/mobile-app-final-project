b8f9830fac538e3b7bb63a5fd7af32143a849a65
osherlev <osherlev317@gmail.com>
2025-02-28 22:01:45 +0200

    add location_icon for preparing for posts

--- app/src/main/java/com/group147/appartmentblog/screens/map/MapFragment.kt
+++ app/src/main/java/com/group147/appartmentblog/screens/map/MapFragment.kt
@@ -1,5 +1,7 @@
 package com.group147.appartmentblog.screens.map
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,11 +11,13 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.group147.appartmentblog.R
 
 class MapFragment : Fragment(), OnMapReadyCallback {
-
     private lateinit var map: GoogleMap
 
     override fun onCreateView(
@@ -26,19 +30,30 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val mapFragment = childFragmentManager.findFragmentById(R.id.googleMapFragment) as SupportMapFragment?
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.googleMapFragment) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         map.mapType = GoogleMap.MAP_TYPE_NORMAL
+        val location = LatLng(37.7749, -122.4194)
+        map.addMarker(
+            MarkerOptions()
+                .position(location)
+                .icon(resizeMapIcon(R.drawable.location_icon, 200, 200))
+        )
 
-        // Move camera to a location
         //todo: get the user location
-        val location = LatLng(37.7749, -122.4194) // San Francisco
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
 
         map.isTrafficEnabled = true
+    }
+
+    fun resizeMapIcon(imageRes: Int, width: Int, height: Int): BitmapDescriptor {
+        val bitmap = BitmapFactory.decodeResource(context?.resources, imageRes)
+        val smallBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false)
+        return BitmapDescriptorFactory.fromBitmap(smallBitmap)
     }
 }
