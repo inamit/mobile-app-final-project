f785a8c2443cff83f3c71167d392930a39664a0d
osherlev <osherlev317@gmail.com>
2025-02-28 17:46:35 +0200

    ctrl alt l + removing un needed stuff + todos

--- app/src/main/java/com/group147/appartmentblog/screens/map/MapFragment.kt
+++ app/src/main/java/com/group147/appartmentblog/screens/map/MapFragment.kt
@@ -4,41 +4,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.group147.appartmentblog.R
-import androidx.fragment.app.Fragment
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_map, container, false)
-        class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
+    }
 
-            private lateinit var map: GoogleMap
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-            override fun onViewCreated(view: android.view.View, savedInstanceState: Bundle?) {
-                super.onViewCreated(view, savedInstanceState)
-                val mapFragment =
-                    childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment?
-                mapFragment?.getMapAsync(this)
-            }
+        val mapFragment = childFragmentManager.findFragmentById(R.id.googleMapFragment) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
 
-            override fun onMapReady(googleMap: GoogleMap) {
-                map = googleMap
-                map.mapType = GoogleMap.MAP_TYPE_NORMAL
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.mapType = GoogleMap.MAP_TYPE_NORMAL
 
-                // Set camera position and zoom
-                val location = LatLng(37.7749, -122.4194) // San Francisco
-                map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
+        // Move camera to a location
+        //todo: get the user location
+        val location = LatLng(37.7749, -122.4194) // San Francisco
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
 
-                map.isTrafficEnabled = true
-            }
-        }
+        map.isTrafficEnabled = true
     }
 }
