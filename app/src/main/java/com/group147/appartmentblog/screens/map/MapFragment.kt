0e19355b3e40e49a298fabe2f207d4c803b18567
osherlev <osherlev317@gmail.com>
2025-02-28 22:41:31 +0200

    putting the base for clicking on posts (will replace the locationData)

--- app/src/main/java/com/group147/appartmentblog/screens/map/MapFragment.kt
+++ app/src/main/java/com/group147/appartmentblog/screens/map/MapFragment.kt
@@ -14,8 +14,10 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.group147.appartmentblog.R
+import kotlin.collections.set
 
 class MapFragment : Fragment(), OnMapReadyCallback {
     private lateinit var map: GoogleMap
@@ -38,17 +40,11 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         map.mapType = GoogleMap.MAP_TYPE_NORMAL
-        val location = LatLng(37.7749, -122.4194)
-        map.addMarker(
-            MarkerOptions()
-                .position(location)
-                .icon(resizeMapIcon(R.drawable.location_icon, 200, 200))
-        )
-
         //todo: get the user location
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
+        val userLocation = LatLng(37.7749, -122.4194)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 12f))
 
-        map.isTrafficEnabled = true
+        showPosts()
     }
 
     fun resizeMapIcon(imageRes: Int, width: Int, height: Int): BitmapDescriptor {
@@ -56,4 +52,40 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         val smallBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false)
         return BitmapDescriptorFactory.fromBitmap(smallBitmap)
     }
+
+    fun showPosts() {
+        val locations = listOf(
+            LocationData("Place 1", LatLng(37.7749, -122.4194)),
+            LocationData("Place 2", LatLng(40.7128, -74.0060)),
+            LocationData("Place 3", LatLng(34.0522, -118.2437))
+        )
+        val markerMap = mutableMapOf<Marker, LocationData>()
+
+        for (location in locations) {
+            val marker = map.addMarker(
+                MarkerOptions()
+                    .position(location.latLng)
+                    .title(location.name)
+                    .icon(resizeMapIcon(R.drawable.location_icon, 200, 200))
+            )
+            marker?.let { markerMap[it] = location } // Store the marker and its data
+        }
+        map.setOnMarkerClickListener { marker ->
+            val locationData = markerMap[marker]
+            marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+            marker.showInfoWindow()
+            locationData?.let {
+                openDetailsScreen(it)
+            }
+            true
+        }
+    }
+
+    // Function to open details screen of post
+    fun openDetailsScreen(locationData: LocationData) {
+        //  val intent = Intent(this, PostActivity::class.java)
+        //  startActivity(intent)
+    }
 }
+//removed when posts exist
+data class LocationData(val name: String, val latLng: LatLng)
