0cde1e839389f0aeb294e5190134dbdfb8d5d8c3
osherlev <osherlev317@gmail.com>
2025-03-07 11:38:45 +0200

    add the post list fragment

---
+++ app/src/main/java/com/group147/appartmentblog/model/PostViewModel.kt
@@ -0,0 +1,23 @@
+package com.group147.appartmentblog.model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.group147.appartmentblog.dao.PostDatabase
+import com.group147.appartmentblog.repositories.PostRepository
+import kotlinx.coroutines.launch
+
+class PostViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: PostRepository = PostRepository(PostDatabase.getDatabase(application).postDao())
+
+    // LiveData to observe posts
+    val allPosts: LiveData<List<Post>> = repository.allPosts
+
+    fun addPost(post: Post) {
+        viewModelScope.launch {
+            repository.insertPost(post)
+        }
+    }
+}
--- app/src/main/java/com/group147/appartmentblog/screens/post/PostFragment.kt
+++ app/src/main/java/com/group147/appartmentblog/screens/post/PostFragment.kt
@@ -1,41 +1,32 @@
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.group147.appartmentblog.R
 import com.group147.appartmentblog.adapters.PostAdapter
 import com.group147.appartmentblog.model.PostViewModel
+import com.group147.appartmentblog.R
 
-class FeedFragment : Fragment() {
+class PostFragment : Fragment() {
     private lateinit var postViewModel: PostViewModel
+    private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: PostAdapter
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_post_list, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView = view.findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
         adapter = PostAdapter() // Initialize with empty list
         recyclerView.adapter = adapter
 
-        // Get ViewModel instance directly
         postViewModel = ViewModelProvider(this).get(PostViewModel::class.java)
 
-        // Observe LiveData for automatic UI updates
+        // Observe LiveData and update RecyclerView when data changes
         postViewModel.allPosts.observe(viewLifecycleOwner) { posts ->
-            posts?.let {
-                adapter.submitList(it)  // Update the adapter's data
-            }
+            adapter.submitList(posts)
         }
-
-        return view
     }
 }
