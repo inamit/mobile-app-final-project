7cdcacec2a3476041b9f916429cdae025245cbc3
Amit Inbar <inamit323@gmail.com>
2025-03-20 19:51:28 +0200

    fix: lifecycle order

--- app/src/main/java/com/group147/appartmentblog/screens/apartment/PostFragment.kt
+++ app/src/main/java/com/group147/appartmentblog/screens/apartment/PostFragment.kt
@@ -108,13 +108,9 @@ class PostFragment : Fragment(), PopupMenu.OnMenuItemClickListener {
     override fun onStop() {
         super.onStop()
         (activity as MainActivity).showAddApartmentButton()
+        (activity as MainActivity).showBottomNavBar()
         (activity as MainActivity).hideToolbarNavigationIcon()
         (activity as MainActivity).hideToolbarMenu()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        (activity as MainActivity).showBottomNavBar()
     }
 
     private fun observePost() {
