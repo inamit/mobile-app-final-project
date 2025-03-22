3da1fca1dbfd72eb926a65b0ed23abe45347b4f8
Benli <e@example.com>
2025-03-22 18:36:41 +0200

    fix chatButton toggle mode

--- app/src/main/java/com/group147/appartmentblog/screens/apartment/PostViewModel.kt
+++ app/src/main/java/com/group147/appartmentblog/screens/apartment/PostViewModel.kt
@@ -102,6 +102,7 @@ class PostViewModel(
             editButton.visibility = if (editMode) View.GONE else View.VISIBLE
             addressTextView.visibility = if (editMode) View.GONE else View.VISIBLE
             saveButton.visibility = if (editMode) View.VISIBLE else View.GONE
+            chatButton.visibility = if (editMode) View.GONE else View.VISIBLE
         }
     }
 
